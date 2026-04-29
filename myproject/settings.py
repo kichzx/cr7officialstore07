162370b471811b37ea59c60b127328539fb1c84d
Mohammed Farsin Sajad <farsinsajad583@gmail.com>
2026-04-29 15:37:40 +0530

    Fix mobile alignment for header

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,22 +12,19 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-g^9zm#4cvq4l4$2oxiz!wdpsywn8t!b8#7a#6*)j*%1o8d!dtt'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
